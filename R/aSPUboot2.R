5e473a986df0d2ad370cd3ad3ebaa642b78388be
ILYOUP KWAK <ikwak2@stat.wisc.edu>
2014-10-08 14:02:04 -0500

    update

--- R/aSPUboot2.R
+++ R/aSPUboot2.R
@@ -115,7 +115,7 @@ aSPUboot2 <- function(Y, X, cov=NULL, model=c("gaussian", "binomial"), pow=c(1:8
     for ( j in 1:length(pow))
     {
         pPerm0[j] = round( sum(abs(Ts[j])<=abs(T0s[,j])) / n.perm, digits = 8)
-        P0s = ( n.perm - rank( abs(T0s[,j]) ) ) / (n.perm - 1)
+        P0s = ( ( n.perm - rank( abs(T0s[,j]) ) ) + 1 ) / (n.perm)
         if (j == 1 ) minp0  = P0s else minp0[which(minp0>P0s)] = P0s[which(minp0>P0s)]
     }
 
--- R/aSPUperm.R
+++ R/aSPUperm.R
@@ -73,8 +73,8 @@ aSPUperm <- function(Y, X, cov = NULL, model=c("gaussian","binomial"), pow=c(1:8
     s <- sample(1:10^5,1)
 
     for (j in 1:length(pow)){
-        set.seed(s) # to ensure the same samples are drawn for each pow
         for (b in 1:n.perm){
+            set.seed(s+b*30) # to ensure the same samples are drawn for each pow
             r0 <- sample(r, length(r))
             U0 <- as.vector(t(XUs) %*% r0)
             if (pow[j] < Inf){ T0s[b] = round(sum( U0^pow[j]), digits = 8) }
@@ -82,7 +82,7 @@ aSPUperm <- function(Y, X, cov = NULL, model=c("gaussian","binomial"), pow=c(1:8
 	}
 
         pPerm0[j] = round( sum(abs(Ts[j])<=abs(T0s)) / n.perm, digits = 8)
-        P0s = (n.perm-rank(abs(T0s)))/(n.perm - 1)
+        P0s = ( (n.perm-rank(abs(T0s))) + 1 )/(n.perm)
         if (j==1) minp0=P0s else minp0[which(minp0>P0s)]=P0s[which(minp0>P0s)]
     }
 
