0ee6bdfbe667e3d24689bfcee1715fad646e7e71
ILYOUP KWAK <ikwak2@stat.wisc.edu>
2014-12-15 21:19:22 -0600

    up

--- R/aSPU.R
+++ R/aSPU.R
@@ -1,6 +1,6 @@
-#' Sum of powered score (SPU) test
+#' Adaptive Sum of powered score (SPU) tests (SPU and aSPU)
 #'
-#' It gives the p-values of the SPU test and aSPU test.
+#' It gives the p-values of the SPU tests and aSPU test.
 #'
 #' @param Y phenotype data. It can be disease lables; =0 for controls, =1 for cases.
 #' or It can be any quantitative traits. Vector with length n (number of observations)
@@ -11,7 +11,7 @@
 #'
 #' @param cov covariates. Matrix with dimension n by k (n :number of observation, k : number of covariates)
 #'
-#' @param resample Use "perm" for residual permutations and "boot" for parametric bootstrap
+#' @param resample Use "perm" for residual permutations, "sim" for simulations from the distribution and "boot" for parametric bootstrap
 #'
 #' @param model Use "gaussian" for quantitative trait, and use "binomial" for binary trait.
 #'
@@ -34,7 +34,7 @@
 #' @seealso \code{\link{aSPUperm}}, \code{\link{aSPUperm2}}, \code{\link{aSPUboot}}, \code{\link{aSPUboot2}}
 
 
-aSPU <- function(Y, X, cov=NULL, resample = c("perm", "boot"), model=c("gaussian", "binomial"), pow = c(1:8, Inf), n.perm = 1000, userank = T ) {
+aSPU <- function(Y, X, cov=NULL, resample = c("perm", "sim", "boot"), model=c("gaussian", "binomial"), pow = c(1:8, Inf), n.perm = 1000, userank = T ) {
 
     model <- match.arg(model)
     resample <- match.arg(resample)
@@ -46,7 +46,15 @@ aSPU <- function(Y, X, cov=NULL, resample = c("perm", "boot"), model=c("gaussian
             aSPUboot2(Y = Y, X = X, cov = cov, pow = pow, n.perm = n.perm, model = model)
         }
     } else {
-        aSPUpermC(Y = Y, X = X, cov = cov, pow = pow, n.perm = n.perm, model = model, userank = userank)
-    }
 
+        if(resample == "sim") {
+            if(n.perm > 10^5) {
+                aSPUsim(Y = Y, X = X, cov = cov, pow = pow, n.perm = n.perm, model = model)
+            } else {
+                aSPUsim2(Y = Y, X = X, cov = cov, pow = pow, n.perm = n.perm, model = model)
+            }
+        } else {
+            aSPUpermC(Y = Y, X = X, cov = cov, pow = pow, n.perm = n.perm, model = model, userank = userank)
+        }
+    }
 }
