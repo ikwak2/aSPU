ff94682d6039f15d0e9b4f1d95624bb20ec6fc5f
ilyoup kwak <ikwak@x-134-84-0-117.vpn.umn.edu>
2015-07-24 15:08:10 -0500

    dataset update

--- R/aSPUMpath.r
+++ R/aSPUMpath.r
@@ -29,11 +29,11 @@
 #' data(kegg9)
 #'
 #' # p-values of SPUpath and aSPUpath tests.
-#' out.a <- aSPUMpath(nZ, varSNP = ldmatrix, pow=c(1:8, Inf),
-#'                   pow2 = c(1,2,4,8), varprop = .95,
-#'                   snp.info=snp.info, gene.info = gene.info,
-#'                   n.perm=100, Ps = TRUE)
-#out.a
+#' out.a <- aSPUMpath(kegg9$nZ, corrSNP = kegg9$ldmatrix, pow=c(1:8, Inf),
+#'                   pow2 = c(1,2,4,8), 
+#'                   snp.info=kegg9$snp.info, gene.info = kegg9$gene.info,
+#'                   n.perm=10, Ps = TRUE)
+#' out.a
 #'
 #' @seealso \code{\link{simPathAR1Snp}} \code{\link{aSPUpathSingle}} \code{\link{aSPUpath}}
 
