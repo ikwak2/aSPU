474e275aaa44007d53ab50451048cdf3f39cb2b9
ilyoup kwak <ikwak@x-10-104-73-96.uofm-secure.wireless.umn.edu>
2015-09-13 18:24:37 -0500

    update

--- src/R_get_pvs_s.c
+++ src/R_get_pvs_s.c
@@ -8,159 +8,34 @@
 #include <R_ext/Utils.h>
 #include "util.h"
 
-# include <complex.h>
-# include <time.h>
-
-
-/******************************************************************************/
-
-float r4_uniform_01 ( int *seed )
-
-/******************************************************************************/
-/*
-  Purpose:
-
-    R4_UNIFORM_01 returns a unit pseudorandom R4.
-
-  Discussion:
-
-    This routine implements the recursion
-
-      seed = 16807 * seed mod ( 2^31 - 1 )
-      r4_uniform_01 = seed / ( 2^31 - 1 )
-
-    The integer arithmetic never requires more than 32 bits,
-    including a sign bit.
-
-    If the initial seed is 12345, then the first three computations are
-
-      Input     Output      R4_UNIFORM_01
-      SEED      SEED
-
-         12345   207482415  0.096616
-     207482415  1790989824  0.833995
-    1790989824  2035175616  0.947702
-
-  Licensing:
-
-    This code is distributed under the GNU LGPL license.
-
-  Modified:
-
-    16 November 2004
-
-  Author:
-
-    John Burkardt
-
-  Reference:
-
-    Paul Bratley, Bennett Fox, Linus Schrage,
-    A Guide to Simulation,
-    Springer Verlag, pages 201-202, 1983.
-
-    Pierre L'Ecuyer,
-    Random Number Generation,
-    in Handbook of Simulation
-    edited by Jerry Banks,
-    Wiley Interscience, page 95, 1998.
-
-    Bennett Fox,
-    Algorithm 647:
-    Implementation and Relative Efficiency of Quasirandom
-    Sequence Generators,
-    ACM Transactions on Mathematical Software,
-    Volume 12, Number 4, pages 362-376, 1986.
-
-    Peter Lewis, Allen Goodman, James Miller,
-    A Pseudo-Random Number Generator for the System/360,
-    IBM Systems Journal,
-    Volume 8, pages 136-143, 1969.
-
-  Parameters:
-
-    Input/output, int *SEED, the "seed" value.  Normally, this
-    value should not be 0.  On output, SEED has been updated.
-
-    Output, float R4_UNIFORM_01, a new pseudorandom variate, strictly between
-    0 and 1.
-*/
+double randn (double mu, double sigma)
 {
-  const int i4_huge = 2147483647;
-  int k;
-  float r;
+  double U1, U2, W, mult;
+  static double X1, X2;
+  static int call = 0;
 
-  k = *seed / 127773;
+  if (call == 1)
+    {
+      call = !call;
+      return (mu + sigma * (double) X2);
+    }
 
-  *seed = 16807 * ( *seed - k * 127773 ) - k * 2836;
+  do
+    {
+      U1 = -1 + ((double) rand () / RAND_MAX) * 2;
+      U2 = -1 + ((double) rand () / RAND_MAX) * 2;
+      W = pow (U1, 2) + pow (U2, 2);
+    }
+  while (W >= 1 || W == 0);
 
-  if ( *seed < 0 )
-  {
-    *seed = *seed + i4_huge;
-  }
-/*
-  Although SEED can be represented exactly as a 32 bit integer,
-  it generally cannot be represented exactly as a 32 bit real number!
-*/
-  r = ( float ) ( *seed ) * 4.656612875E-10;
+  mult = sqrt ((-2 * log (W)) / W);
+  X1 = U1 * mult;
+  X2 = U2 * mult;
 
-  return r;
+  call = !call;
+
+  return (mu + sigma * (double) X1);
 }
-
-
-
-/******************************************************************************/
-
-float r4_normal_01 ( int *seed )
-
-/******************************************************************************/
-/*
-  Purpose:
-
-    R4_NORMAL_01 returns a unit pseudonormal R4.
-
-  Discussion:
-
-    The standard normal probability distribution function (PDF) has
-    mean 0 and standard deviation 1.
-
-    The Box-Muller method is used, which is efficient, but
-    generates two values at a time.
-
-  Licensing:
-
-    This code is distributed under the GNU LGPL license.
-
-  Modified:
-
-    05 June 2013
-
-  Author:
-
-    John Burkardt
-
-  Parameters:
-
-    Input/output, int *SEED, a seed for the random number generator.
-
-    Output, float R4_NORMAL_01, a normally distributed random value.
-*/
-{
-  float r1;
-  float r2;
-  const double r4_pi = 3.141592653589793;
-  float x;
-
-  r1 = r4_uniform_01 ( seed );
-  r2 = r4_uniform_01 ( seed );
-  x = sqrt ( - 2.0 * log ( r1 ) ) * cos ( 2.0 * r4_pi * r2 );
-
-  return x;
-}
-
-
-
-
 
 
 void get_pvs_s(double *Zs, double *CovSsqrt, double *Ts,
@@ -195,7 +70,7 @@ void get_pvs_s(double *Zs, double *CovSsqrt, double *Ts,
     //    printf("Random smp :");
 
     for(j = 0 ; j < n_Zs ; j++ ) {
-      nR[j] = r4_normal_01(&seeds[i*n_Zs + j]);
+      nR[j] = randn(0,1);
       //      printf("%f  ", nR[j]);
     }
     //    printf("\n");
