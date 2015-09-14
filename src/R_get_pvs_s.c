62b4bc81862e79f6bf045eb5458270e62046fd17
ilyoup kwak <ikwak@x-134-84-3-70.vpn.umn.edu>
2015-09-14 12:07:40 -0500

    test

--- src/R_get_pvs_s.c
+++ src/R_get_pvs_s.c
@@ -28,7 +28,7 @@ void get_pvs_s(double *Zs, double *Ts,
   pPerm0 = (double *) R_alloc ( n_pow, sizeof(double) ) ;
   T0s = (double *) R_alloc ( n_pow, sizeof(double) ) ;
   U0 = (double *) R_alloc ( n_Zs, sizeof(double) ) ;
-%  P0s = (double *) R_alloc ( n_perm * n_pow, sizeof(double) ) ;
+  P0s = (double *) R_alloc ( n_perm * n_pow, sizeof(double) ) ;
   minP0s = (double *) R_alloc ( n_perm, sizeof(double) ) ;
 
 
