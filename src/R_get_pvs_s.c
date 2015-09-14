8f09fec7e63c588c03eda32c63f9c06757e66a71
ilyoup kwak <ikwak@x-134-84-3-70.vpn.umn.edu>
2015-09-14 11:57:20 -0500

    test

--- src/R_get_pvs_s.c
+++ src/R_get_pvs_s.c
@@ -26,7 +26,7 @@ void get_pvs_s(double *Zs, double *Ts,
   double *pPerm0, *T0s, *U0, ss, *P0s, *minP0s, minpPerm0, minp;
 
   pPerm0 = (double *) R_alloc ( n_pow, sizeof(double) ) ;
-  T0s = (double *) R_alloc ( n_pow, sizeof(double) ) ;
+  T0s = (double *) R_alloc ( n_perm * n_pow, sizeof(double) ) ;
   U0 = (double *) R_alloc ( n_Zs, sizeof(double) ) ;
   P0s = (double *) R_alloc ( n_perm * n_pow, sizeof(double) ) ;
   minP0s = (double *) R_alloc ( n_perm, sizeof(double) ) ;
@@ -54,7 +54,7 @@ void get_pvs_s(double *Zs, double *Ts,
 	    ss = fabs(U0[b]);
 	}
       }
-      T0s[j] = ss;
+      T0s[i*n_pow + j] = ss;
     }
   }
   //  printf("\n");
@@ -69,7 +69,7 @@ void get_pvs_s(double *Zs, double *Ts,
   for( j = 0 ; j < n_pow ; j++ ) {
     ss = 0;
     for( i = 0 ; i < n_perm ; i++) {
-      if ( fabs(Ts[j]) <= fabs(T0s[ j ]) )
+      if ( fabs(Ts[j]) <= fabs(T0s[ i*n_pow + j ]) )
 	ss += 1;
 
       //      printf("%f  ", T0s[i*n_pow + j ] );
@@ -87,19 +87,42 @@ void get_pvs_s(double *Zs, double *Ts,
       ss = 0;
       for( k = 0 ; k < n_perm ; k++) {
 	if(k != i) {
-	  if ( fabs(T0s[ j]) <= fabs(T0s[ j]) )
+	  if ( fabs(T0s[ i*n_pow + j]) <= fabs(T0s[ k*n_pow + j]) )
 	    ss++;
 	}
       }
       P0s[ i*n_pow + j] = (ss + 1) / n_perm ;
     }
   }
+  //  if (j==1) minp0=P0s else minp0[which(minp0>P0s)]=P0s[which(minp0>P0s)]
+  for(i = 0 ; i < n_perm ; i++ ) {
+    minP0s[i] = P0s[i*n_pow + 0]  ;
+  }
+  for(j = 1 ; j < n_pow ; j ++) {
+    for(i = 0 ; i < n_perm ; i++ ) {
+      if(minP0s[i] > P0s[i*n_pow + j])
+	minP0s[i] = P0s[i*n_pow + j];
+    }
+  }
+
+  // minp = (sum( min(pPerm0) >= minP0s )+1) / (B+1)
+  ss = 1;
+  for(i=0 ; i < n_pow; i++) {
+    if( ss > pPerm0[i] )
+      ss = pPerm0[i];
+  }
+  minpPerm0 = ss;
+
+  ss = 0;
+  for(i = 0; i < n_perm; i++ ) {
+    if(minpPerm0 >= minP0s[i])
+      ss += 1;
+  }
+  minp = (ss+1) / (n_perm+1);
 
   // pvs <- c(pPerm0, minP)
   for(i = 0 ; i < n_pow; i++ ) {
-    pvs[i] = i;
+    pvs[i] = pPerm0[i];
   }
-  pvs[n_pow] = 1;
+  pvs[n_pow] = minp;
 }
-
-
