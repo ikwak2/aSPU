a46d2d6be5b4de794c839744baca0025b16124bf
ilyoup kwak <ikwak@umn.edu>
2016-04-28 16:34:04 -0500

    MTaSPUsSetScore test manual update

--- R/MTaSPUsSetScore.R
+++ R/MTaSPUsSetScore.R
@@ -2,7 +2,7 @@
 #'
 #' It gives p-values of the MTSPUsSet tests MTScore and MTaSPUsSet_Score test with GWAS summary statistics.
 #'
-#' @param Zs Z-scores for each SNPs. It could be P-values if the Ps option is TRUE. 
+#' @param Zs Z-score matrix. row represent SNPs and column represent traits. It could be P-values if the Ps option is TRUE. 
 #'
 #' @param corSNP Correlation matirx of the SNPs to be tested; estimated from a
 #' reference panel (based on the same set of the reference alleles as
@@ -10,9 +10,9 @@
 #'
 #' @param corPhe Correlation matirx of phenotypes to be tested; Estimated from Z-scores.
 #' 
-#' @param pow SNP specific power(gamma values) used in MTSPUsSet test.
+#' @param pow SNP specific power(gamma values) used in MTSPUsSetScore test.
 #'
-#' @param pow2 GENE specific power(gamma values) used in MTSPUsSet test.
+#' @param pow2 GENE specific power(gamma values) used in MTSPUsSetScore test.
 #'
 #' @param n.perm number of permutations or bootstraps.
 #'
