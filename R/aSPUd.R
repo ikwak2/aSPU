17b6aaf1b3d32fdb3a6012cbe880e4883a151cce
ilyoup kwak <ikwak@umn.edu>
2016-05-26 15:45:57 -0500

    aSPUd added

---
+++ R/aSPUd.R
@@ -0,0 +1,87 @@
+## Adaptive Sum of powered score (SPU) tests (SPU and aSPU) (distribution based)
+##
+## It gives the p-values of the SPU(1), SPU(2) and minP tests and aSPU test based on the distribution of them
+##
+## @param Y phenotype data. It can be disease lables; =0 for controls, =1 for cases.
+##     or It can be any quantitative traits. Vector with length n (number of observations)
+##
+## @param X genotype data; each row for a subject, and each column
+##     for an SNP. The value of each element is the # of the copies
+##     for an allele. Matrix with dimension n by k (n : number of observation, k : number of genotype data)
+##
+## @param cov covariates. Matrix with dimension n by p (n :number of observation, p : number of covariates)
+##
+## @param model Use "gaussian" for quantitative trait (Default)
+##    , and Use "binomial" for binary trait.
+##
+##
+## @export
+## @return p-values for SPU(1), SPU(2), minP tests and aSPU test.
+##
+## @examples
+##
+## data(exdat)
+## out <- aSPUd(exdat$Y, exdat$X, cov = NULL, model = "binomial")
+## out
+##
+## @seealso \code{\link{aSPU}}
+
+
+aSPUd <- function(Y, X, cov = NULL, model=c("gaussian","binomial") ){
+
+    model = match.arg(model)
+
+    n <- length(Y)
+    if (is.null(X) && length(X)>0) X=as.matrix(X, ncol=1)
+    k <- ncol(X)
+
+#### Score vector:
+    if (is.null(cov)){
+        ## NO nuisance parameters:
+        XUs<-Xg <- X
+        r<-Y-mean(Y)
+        U<-as.vector(t(Xg) %*% r)
+
+        Xbar<-apply(Xg, 2, mean)
+        Xgb<-Xg
+        for(i in 1:nrow(Xg))
+            Xgb[i,]<-Xg[i,]-Xbar
+
+	if( model == "binomial" ) {
+            CovS <- mean(Y)*(1-mean(Y))*(t(Xgb) %*% Xgb)
+	} else {
+            CovS <- var(Y)*(t(Xgb) %*% Xgb)
+	}
+    } else {
+        tdat1<-data.frame(trait=Y, cov)
+        fit1<-glm(trait~.,family=model,data=tdat1)
+        pis<-fitted.values(fit1)
+        XUs<-matrix(0, nrow=n, ncol=k)
+        Xmus = X
+        for(i in 1:k){
+            tdat2<-data.frame(X1=X[,i], cov)
+            fit2<-glm(X1~.,data=tdat2)
+            Xmus[,i]<-fitted.values(fit2)
+            XUs[, i]<-(X[,i] - Xmus[,i])
+        }
+        r<-Y - pis
+        U<-t(XUs) %*% r
+        
+        if( model == "binomial" ) {
+            CovS <- mean(pis*(1-pis))*(t(Xgb) %*% Xgb)
+        } else {
+            CovS <- var(r)*(t(Xgb) %*% Xgb)
+        }
+    }
+
+    p1 = SumSqU(U, CovS)
+    p2 = Sum(U, CovS)
+    p3 = UminPd(U, CovS)
+    mp = min(p1,p2,p3)
+    p = 1 - (1-mp)^3
+             
+    pvs = c(p1,p2,p3,p)
+    names(pvs) = c("Psum","Pssu","PminP","PaSPU")
+    pvs
+}
+
--- R/util.R
+++ R/util.R
@@ -1,3 +1,76 @@
+### SSU #################
+
+
+SumSqU<-function(U, CovS){
+    if (is.null(dim(CovS))) {# only one-dim:
+        Tscore<- sum(U^2 /CovS)
+        if (is.na(Tscore) || is.infinite(Tscore) || is.nan(Tscore)) Tscore<-0
+        pTg1<-as.numeric(1-pchisq(Tscore, 1))
+    }
+         else {
+                                        #it's possible U=0 and Cov(U)=0:
+             if (all(abs(U)<1e-20)) pTg1<-1 else{
+                                                Tg1<- t(U) %*% U
+                                                ##distr of Tg1 is sum of cr Chisq_1:
+                                                cr<-eigen(CovS, only.values=TRUE)$values
+                                                ##approximate the distri by alpha Chisq_d + beta:
+                                                alpha1<-sum(cr*cr*cr)/sum(cr*cr)
+                                                beta1<-sum(cr) - (sum(cr*cr)^2)/(sum(cr*cr*cr))
+                                                d1<-(sum(cr*cr)^3)/(sum(cr*cr*cr)^2)
+                                                alpha1<-as.double(alpha1)
+                                                beta1<-as.double(beta1)
+                                                d1<-as.double(d1)
+                                                pTg1<-as.numeric(1-pchisq((Tg1-beta1)/alpha1, d1))
+                                            }
+         }
+    return(pTg1)
+}
+
+##########SumTest########################
+Sum<-function(U, CovS){
+                                        #it's possible U=0 and Cov(U)=0:
+    if (all(abs(sum(U))<1e-20)) pTsum<-1 else{
+                                             a<-rep(1, length(U))
+                                             Tsum<- sum(U)/(sqrt(as.numeric(t(a) %*% CovS %*% (a))))
+                                             pTsum <- as.numeric( 1-pchisq(Tsum^2, 1) )
+                                         }
+    pTsum
+}
+
+##########UminP Test########################
+UminPd<-function(U, CovS){
+
+    if (is.null(dim(CovS))) {# only one-dim:
+        Tu<- sum(U^2 /CovS)
+        if (is.na(Tu) || is.infinite(Tu) || is.nan(Tu)) Tu<-0
+        pTu<-as.numeric(1-pchisq(Tu, 1))
+    }
+         else{
+####it's POSSIBLR Ui=0 and CovS[i,i]=0!
+             Tu<-as.vector(abs(U)/(sqrt(diag(CovS)) + 1e-20) )
+             k<-length(U)
+             V <- matrix(0,nrow=k, ncol=k)
+             for(i in 1:k){
+                 for(j in 1:k){
+                     if (abs(CovS[i,j])>1e-20)
+                         V[i,j] <- CovS[i,j]/sqrt(CovS[i,i]*CovS[j,j])
+                     else   V[i,j] <- 1e-20
+                 }
+             }
+             pTu <- as.numeric(PowerUniv(Tu,V))
+         }
+    
+    pTu
+}
+
+PowerUniv <- function(U,V){
+    n <- dim(V)[1]
+
+    x <- as.numeric(max(abs(U)))
+    TER <- as.numeric(1-pmvnorm(lower=c(rep(-x,n)),upper=c(rep(x,n)),mean=c(rep(0,n)),sigma=V))
+
+    return(TER)
+}
 
 ###extract the first fw PCs such that they explain at least cutoff*100%
 ### of total variation from X;
