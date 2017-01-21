2038187fcc0fd67bf669c43346a05bc79d6aafab
ilyoup kwak <ikwak@umn.edu>
2017-01-21 11:33:00 -0600

    cpp version of aSPUsPath updated

--- R/aSPUsPathC.r
+++ R/aSPUsPathC.r
@@ -36,7 +36,7 @@
 #' data(kegg9)
 #'
 #' # p-values of SPUpath and aSPUpath tests.
-#' out.a <- aSPUsPath(kegg9$nP, corSNP = kegg9$ldmatrix, pow=c(1:8, Inf),
+#' out.a <- aSPUsPathC(kegg9$nP, corSNP = kegg9$ldmatrix, pow=c(1:8, Inf),
 #'                   pow2 = c(1,2,4,8), 
 #'                   snp.info=kegg9$snp.info, gene.info = kegg9$gene.info,
 #'                   n.perm=10, Ps = TRUE)
